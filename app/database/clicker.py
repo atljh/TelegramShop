b0faed6570e9222a06e6d9751e482ba9070248a2
Fiodor <fiodorlukashov@gmail.com>
2024-07-21 19:43:12 +0000

    Add check if user have energy

--- app/database/clicker.py
+++ app/database/clicker.py
@@ -36,17 +36,34 @@ async def get_bot_user(telegram_id: int, conn: Connection) -> dict:
     user = await conn.fetchrow(q, telegram_id)
     return dict(user) if user else None
 
+@connection
+async def get_user_energy(telegram_id: int, conn: Connection):
+    q = '''
+    SELECT 
+        energy_amount 
+    FROM 
+        bot_user 
+    WHERE 
+        bot_user.telegram_id = $1
+    '''
+    energy_amount = await conn.fetchval(q, telegram_id)
+    return energy_amount > 0
 
 @connection
 async def tap(telegram_id: int, conn: Connection):
     xcoins_for_click = await var.get_var('xcoins_for_click', int)
     
+    user_have_energy = await get_user_energy(telegram_id)
+    if not user_have_energy:
+        return get_bot_user(telegram_id)
     q = '''
     UPDATE bot_user
     SET xcoins = xcoins + $1,
-        energy_amount = energy_amount - 1
+        energy_amount = energy_amount - 1 
+    WHERE telegram_id = $2;
     '''
-    await conn.execute(q, xcoins_for_click)
+    await conn.execute(q, xcoins_for_click, telegram_id)
+    return get_bot_user(telegram_id)
 
 @connection
 async def update_energy(conn: Connection):
